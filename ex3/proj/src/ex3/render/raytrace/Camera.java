836dd417cb585604759b628995fdfcc7cecda5c5
Barak Jacob <baonly@gmail.com>
2012-04-20 16:06:46 +0300

    cam

--- ex3/proj/src/ex3/render/raytrace/Camera.java
+++ ex3/proj/src/ex3/render/raytrace/Camera.java
@@ -8,7 +8,26 @@ import math.Vec;
  * Represents the scene's camera.
  * 
  */
+
 public class Camera {
+
+	//instance variables
+	private Point3D eye = null;
+	private Vec direction = null;
+	private Vec up_direction = null;
+	private int screen_width;
+	private int screen_dist;
+
+
+
+	public Camera (Point3D eye, Vec direction, Vec up_direction, int screen_width, int screen_dist) {
+		this.eye = eye;
+		this.direction = direction;
+		this.up_direction = up_direction;
+		this.screen_width = this.screen_width;
+		this.screen_dist = screen_dist;
+	}
+
 
 	/**
 	 * Transforms image xy coordinates to view pane xyz coordinates. Returns the
@@ -19,7 +38,9 @@ public class Camera {
 	 * @return
 	 */
 	public Ray constructRayThroughPixel(int x, int y) {
-
+		
+		Vec right_direction = Vec.crossProd(up_direction, direction);
+		Poi
 		// Note - this is a trivial Orthographic camera
 		return new Ray(new Point3D(x, y, 0), new Vec(0, 0, -1));
 	}
