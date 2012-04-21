916909fb9a814f91a46c068e562f12e6af826f61
ronreiter <ron.reiter@gmail.com>
2012-04-21 15:42:58 +0300

    trimesh

--- ex3/proj/src/ex3/render/raytrace/TriMesh.java
+++ ex3/proj/src/ex3/render/raytrace/TriMesh.java
@@ -1,9 +1,15 @@
 package ex3.render.raytrace;
 
+import math.Point3D;
 import math.Ray;
 import math.Vec;
 
+import java.util.List;
+import java.util.Map;
+
 public class TriMesh extends Object3D {
+    List<Point3D[]> mesh;
+
     @Override
     public double nearestIntersection(Ray ray) {
         return 0;  //To change body of implemented methods use File | Settings | File Templates.
@@ -12,5 +18,9 @@ public class TriMesh extends Object3D {
     @Override
     public Vec normalAt(Vec intersection, Ray ray) {
         return null;  //To change body of implemented methods use File | Settings | File Templates.
+    }
+    
+    public void init(Map<String, String> attributes) {
+
     }
 }
